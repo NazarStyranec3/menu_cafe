4f30c3ed62006b8d911e8846edd2c0c982796602
NazarStyranec3 <nazarsturanec@gmail.com>
2025-08-10 15:16:05 +0300

    Опис змін, наприклад: Додано підключення стилів і налаштування статики

--- Cofe_Menu/urls.py
+++ Cofe_Menu/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import os
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # приклад твоїх app урлів
+    path('', include('main.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'main', 'static'))
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
